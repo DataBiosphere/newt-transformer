7de0d287e6561ffd60101c9a25b42a77855b47f6
Michael Baumann <mbaumann@ucsc.edu>
2018-10-15 05:03:14 -0700

    Sanitize metadata using typed values
    Retaining the types of sanitized values now will
    help with a smoother transition (of indexing, etc.)
    to the full/unsanitized data.

--- scripts/data_bleach.py
+++ scripts/data_bleach.py
@@ -40,6 +40,23 @@ def is_protected_field(key, context):
     return not any([context + [key] == field for field in PUBLIC_FIELDS])
 
 
+def get_sanitized_value(value):
+    result = None
+    if value is None:
+        result = None
+    elif isinstance(value, str):
+        result = "--------" if len(value) > 0 else ""
+    elif isinstance(value, bool):
+        result = False
+    elif isinstance(value, float):
+        result = 0.0
+    elif isinstance(value, int):
+        result = 0
+    elif isinstance(value, list):
+        result = []
+    return result
+
+
 def recursive_clear(d, context=None):
     """
     clear the values of a dictionary unless the value is another dictionary
@@ -54,7 +71,7 @@ def recursive_clear(d, context=None):
             recursive_clear(val, context=context + [key])
         else:
             if is_protected_field(key, context):
-                d[key] = ''
+                d[key] = get_sanitized_value(d[key])
 
 
 def sanitize_bundle(bundle):
